5f1f5547b2c7427ca895abdde74a2f9b82563da4
raviipawar <ravibalajipawar@gmail.com>
2023-08-18 17:59:23 +0530

    api gateway

--- src/main/java/com/learning/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/com/learning/apigateway/ApiGatewayConfiguration.java
@@ -10,10 +10,8 @@ public class ApiGatewayConfiguration {
 
 	@Bean
 	public RouteLocator gateWayRouter(RouteLocatorBuilder builder) {
-		return builder.routes().route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
-				.route(p -> p.path("/currency-converter-new/**")
-						.filters(f -> f.rewritePath("currency-converter-new/(?<segment>.*)", "currency-converter-feign/${segment}"))
-						.uri("lb://currency-conversion"))
-				.route(p -> p.path("/currency-converter-feign/**").uri("lb://currency-conversion")).build();
+		return builder.routes().route(p -> p.path("/currency-exchanges/**").uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-converter-feign/**").uri("lb://currency-conversion"))
+				.route(p -> p.path("/user-service/all/**").uri("lb://user-service")).build();
 	}
 }
